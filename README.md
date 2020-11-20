737ec60d1e2edded56af81f19fd155a401d789be
William Hearn <sylus1984@gmail.com>
2020-11-20 11:02:52 -0500

    feat(initial): Baseline

---
+++ README.md
@@ -0,0 +1,23 @@
+# Minio
+
+Install and configure the [MinIO operator](https://github.com/minio/minio-operator) using Kustomize.
+
+## Tenants
+
+* Standard // HDD @ 2000Gi
+* Premium // SSD @ 200Gi
+
+## Domains
+
+* minio-standard-tenant-1.example.ca
+* minio-console-standard-tenant-1.example.ca
+* minio-premium-tenant-1.example.ca
+* minio-console-premium-tenant-1.example.ca
+
+## References
+
+* https://github.com/minio/minio
+* https://github.com/minio/minio-operator
+* https://github.com/minio/console
+* https://docs.min.io/docs/minio-sts-quickstart-guide
+* https://github.com/minio/minio/blob/master/docs/sts/opa.md
---
+++ params.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Default Credentials
+export STANDARD_TENANT_1="${STANDARD_TENANT_1:=DEFAULT}"
+export PREMIUM_TENANT_1="${PREMIUM_TENANT_1:=DEFAULT}"
+export OPENID_CLIENT_ID="${OPENID_CLIENT_ID:=default}"
+export OPENID_CONFIG_URL="${OPENID_CONFIG_URL:-default}"
+export CONFIG_FILE="${CONFIG_FILE:-instances/base/tenant.yaml}"
+export CONSOLE_HMAC_JWT_SECRET="${CONSOLE_HMAC_JWT_SECRET:-YOURJWTSIGNINGSECRET}"
+export CONSOLE_PBKDF_PASSPHRASE="${CONSOLE_PBKDF_PASSPHRASE:-SECRET}"
+export CONSOLE_PBKDF_SALT="${CONSOLE_PBKDF_SALT:-SECRET}"
+export CONSOLE_ACCESS_KEY="${CONSOLE_ACCESS_KEY:-YOURCONSOLEACCESS}"
+export CONSOLE_SECRET_KEY="${CONSOLE_SECRET_KEY:-YOURCONSOLESECRET}"
+
+envsubst < instances/standard/tenant-1/secret-console.tmpl > instances/standard/tenant-1/secret-console.txt
+envsubst < instances/standard/tenant-1/secret-minio.tmpl > instances/standard/tenant-1/secret-minio.txt
+envsubst < instances/premium/tenant-1/secret-console.tmpl > instances/premium/tenant-1/secret-console.txt
+envsubst < instances/premium/tenant-1/secret-minio.tmpl > instances/premium/tenant-1/secret-minio.txt
+
+# OIDC
+yq -d5 w -i ${CONFIG_FILE} 'spec.env[2].value' ${OPENID_CLIENT_ID}
+yq -d5 w -i ${CONFIG_FILE} 'spec.env[3].value' ${OPENID_CONFIG_URL}
+
+# Domain
+export DOMAIN_NAME="${DOMAIN_NAME:=default.example.ca}"
+for patch in instances/*/tenant*/patch-minio-ing*; do
+  val=$(yq r $patch '[0].value')
+  yq w -i $patch '[0].value' ${val/example.ca/$DOMAIN_NAME}
+done
+for patch in instances/*/tenant*/patch-console-ing*; do
+  val=$(yq r $patch '[0].value')
+  yq w -i $patch '[0].value' ${val/example.ca/$DOMAIN_NAME}
+done
